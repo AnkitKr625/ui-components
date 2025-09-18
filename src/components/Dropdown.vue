428a8a4741126c24f7a990aabdc7ffe6736e3bea
Ankit Kumar <ankitkr1009@gmail.com>
2025-09-18 10:51:25 +0530

    project cleanup

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Dropdown",
+  props: {
+    msg: {
+      type: String,
+      default: "Hello",
+    },
+  },
+  setup() {
+    const count = ref(0);
+    return {
+      count,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
